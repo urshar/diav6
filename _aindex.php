d5761a7da7ccda71fdd552cfaa35bd10c428d6fb
Andreas Steiner <astone@stoneship.at>
2013-12-21 16:52:10 +0100

    first commit

---
+++ _aindex.php
@@ -0,0 +1,12 @@
+<?php
+/*----------------- php source file -----------------
+  @file      : _aindex.php
+  belongs to : mysql DB
+  contact    : Andreas Steiner   (astone@stoneship.at)
+  @copyright 2013
+  <!-- phpDesigner :: Timestamp [21.12.2013 16:24:51] -->
+  ------HoC------
+ ----------------------------------------------------*/
+
+
+?>
